2680610e7552a7abe53e062abcbd81b3ad3a0e6f
Braeden Kilburn <braedenk99@gmail.com>
2022-09-20 19:54:04 -0600

    Initial Commit

---
+++ Blackjack/BlackjackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackApp.swift
+//  Blackjack
+//
+//  Created by Braeden Kilburn on 9/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BlackjackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
